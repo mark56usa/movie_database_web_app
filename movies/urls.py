dfcfabfa7bfaba4fe56d33032f86955234b66d7b
mark56usa <mark56usa@gmail.com>
2018-02-13 11:29:22 +0000

    Django movie web app

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('create/', views.create, name='create'),
+    path('edit/<str:movie_id>', views.edit, name="edit"),
+    path('delete/<str:movie_id>', views.delete, name="delete")
 ]
--- movies/views.py
+++ movies/views.py
@@ -19,11 +19,41 @@ def create(request):
     if request.method == 'POST':
         data = {
             'Name': request.POST.get('name'),
-            'Pictures': [{'url': request.POST.get('url')}] ,
+            'Pictures': [{'url': request.POST.get('url') or 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/No_image_available_400_x_600.svg/320px-No_image_available_400_x_600.svg.png'}] ,
+            'Rating': int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes'),
+        }
+        try:
+            response=AT.insert(data)
+            messages.success(request, 'Your entry for {} has been added'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request, 'The error {} was generated when attempting create entry'.format(e))
+
+    return redirect("/")
+
+def edit(request, movie_id):
+    if request.method == 'POST':
+        data = {
+            'Name': request.POST.get('name'),
+            'Pictures': [{'url': request.POST.get('url') or 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/No_image_available_400_x_600.svg/320px-No_image_available_400_x_600.svg.png'}],
             'Rating': int(request.POST.get('rating')),
             'Notes': request.POST.get('notes'),
         }
 
-        AT.insert(data)
+        try:
+            response=AT.update(movie_id, data)
+            messages.success(request, '{} has been updated'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request, 'The error {} was generated when attempting update the movie'.format(e))
 
-        return redirect("/")
+    return redirect("/")
+
+def delete(request, movie_id):
+    try:
+        movie_name=AT.get(movie_id)['fields'].get('Name')
+        response=AT.delete(movie_id)
+        messages.warning(request, 'Then entry for {} has been deleted'.format(movie_name))
+    except Exception as e:
+        messages.warning(request, 'The error {} was generated when attempting to delete the record.'.format(e))
+
+    return redirect("/")
